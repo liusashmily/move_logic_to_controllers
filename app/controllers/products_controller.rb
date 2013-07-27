a6282284cccb83bb272cf28fd6330b9ad3e7e408
liusashmily <liusashmily@gmail.com>
2013-07-27 10:27:18 -0500

    section 1

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,26 @@
 class ProductsController < ApplicationController
+	def index
+		@products = Product.all
+
+		respond_to do |format|
+			format.html # show default view
+			format.json {render :json => @products}
+		end
+	end
+
+	def create
+		@product = Product.new(params[:product])
+			if @product.save
+				respond_to do |format|
+					format.html {render :action => "create"}
+					format.json {render :json => @product}
+				end
+			else
+				respond_to do |format|
+					format.html {render :action => "new"}
+					format.json {render :json => @product.errors, :status => :unprocessable_entity}
+				end
+			end
+
+	end
 end
